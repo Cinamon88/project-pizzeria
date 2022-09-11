178b0fd9d45bd944871bc204daca719ebd246193
Lukasz Gorka <lukaszgorka@hotmail.com>
2022-09-11 17:23:55 +0200

    add action to initOrderForm

--- src/js/script.js
+++ src/js/script.js
@@ -130,6 +130,22 @@
     initOrderForm(){
       const thisProduct = this;
 
+      thisProduct.form.addEventListener('submit', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+
+      for(let input of thisProduct.formInputs){
+        input.addEventListener('change', function(){
+          thisProduct.processOrder();
+        });
+      }
+
+      thisProduct.cartButton.addEventListener('click', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+      
       console.log(thisProduct, this.initOrderForm);
     }
    
