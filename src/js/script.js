2b5d7f0b465c034124ecfb59f53b68ac1703ec70
Lukasz Gorka <lukaszgorka@hotmail.com>
2022-09-12 22:25:20 +0200

    update setValue(minValue maxValue)

--- src/js/script.js
+++ src/js/script.js
@@ -257,12 +257,25 @@
       const thisWidget = this;
 
       const newValue = parseInt(value);
+      const minValue = settings.amountWidget.defaultMin;
+      const maxValue = settings.amountWidget.defaultMax;
 
-      /* TODO: Add validation */
+      thisWidget.value = settings.amountWidget.defaultValue;
+
+      /* Add validation */
       if(thisWidget.value !== newValue && !isNaN(newValue)){
         thisWidget.value = newValue;
+      } 
+
+      if(thisWidget.value < minValue){
+        thisWidget.value = minValue;
       }
 
+      if(thisWidget.value > maxValue){
+        thisWidget.value = maxValue;
+      }
+
+      
       thisWidget.input.value = thisWidget.value;
     }
 
