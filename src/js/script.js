422a6ac403d59b3426fb6b59ccbb08b167c1e600
Lukasz Gorka <lukaszgorka@hotmail.com>
2022-09-13 16:32:38 +0200

    add class Cart

--- src/js/script.js
+++ src/js/script.js
@@ -344,6 +344,37 @@
     }
   }
 
+  class Cart{
+    constructor(element){
+      const thisCart = this;
+
+      thisCart.products = [];
+
+      thisCart.getElements(element);
+      thisCart.initActions();
+
+      console.log('new Cart', thisCart);
+    }
+
+    getElements(element){
+      const thisCart =this;
+
+      thisCart.dom = {};
+
+      thisCart.dom.wrapper = element;
+
+      thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+    }
+
+    initActions(){
+      const thisCart = this;
+
+      thisCart.dom.toggleTrigger.addEventListener('click', function(){
+        thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+      });
+
+    }
+  }
 
   const app = {
 
@@ -365,6 +396,12 @@
       console.log('testProduct:', testProduct); */
     },
 
+    initCart: function(){
+      const thisApp = this;
+
+      const cartElem = document.querySelector(select.containerOf.cart);
+      thisApp.cart = new Cart(cartElem);
+    },
 
     init: function(){
       const thisApp = this;
@@ -376,7 +413,10 @@
       
       thisApp.initData();
       thisApp.initMenu();
+      thisApp.initCart();
     },
+
+    
 
   };
 
