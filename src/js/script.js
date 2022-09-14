2b502d837a24811bccb4efdf691a52756dbe020d
Lukasz Gorka <lukaszgorka@hotmail.com>
2022-09-14 11:05:38 +0200

    update Cart

--- src/js/script.js
+++ src/js/script.js
@@ -395,7 +395,9 @@
     announce(){
       const thisWidget = this;
 
-      const event = new Event('updated');
+      const event = new CustomEvent('updated', {
+        bubbles: true
+      });
       thisWidget.element.dispatchEvent(event);
     }
   }
@@ -431,6 +433,10 @@
 
       thisCart.dom.toggleTrigger.addEventListener('click', function(){
         thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+      });
+
+      thisCart.dom.productList.addEventListener('updated', function(){
+        thisCart.update();
       });
     }
 
