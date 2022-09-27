948da9456ed59fa127ef4ff0df45489e8dd94de5
Lukasz Gorka <lukaszgorka@hotmail.com>
2022-09-27 10:16:33 +0200

    add widnow location hash

--- src/js/app.js
+++ src/js/app.js
@@ -12,6 +12,23 @@ const app = {
     thisApp.navLinks = document.querySelectorAll(select.nav.links);
     thisApp.activatePage(thisApp.pages[0].id);
 
+    for(let link of thisApp.navLinks){
+      link.addEventListener('click', function(event){
+        const clickedElement = this;
+        event.preventDefault();
+
+        /* get page id from href attribute */
+        const id = clickedElement.getAttribute('href').replace('#', ''); 
+        
+        /* run thisApp.activatePage with that id */
+        thisApp.activatePage(id);
+
+        /* change URL hash */
+        window.location.hash = '#/' + id;
+
+      });
+    }
+
     
   },
 
