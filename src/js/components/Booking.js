4dd9c57b7c35c7b6c9b2a46869ae74fdd673c135
Lukasz Gorka <lukaszgorka@hotmail.com>
2022-09-29 12:27:06 +0200

    update fix table after chosen date and hour

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -29,13 +29,12 @@ class Booking {
     };
 
     const urls = {
-      booking: `${settings.db.url}/${settings.db.booking}?${params.booking.join(
-        '&'
-      )}`,
-      eventsCurrent: `${settings.db.url}/${settings.db.event
-      }?${params.eventsCurrent.join('&')}`,
-      eventsRepeat: `${settings.db.url}/${settings.db.event
-      }?${params.eventsRepeat.join('&')}`,
+      booking:       settings.db.url + '/' + settings.db.booking 
+                                     + '?' + params.booking.join('&'),
+      eventsCurrent: settings.db.url + '/' + settings.db.event  
+                                     + '?' + params.eventsCurrent.join('&'),
+      eventsRepeat:  settings.db.url + '/' + settings.db.event   
+                                     + '?' + params.eventsRepeat.join('&'),
     };
 
     Promise.all([
