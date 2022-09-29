3dcabeedb7955b3de95d84984a0ebb4a3a6316f3
Lukasz Gorka <lukaszgorka@hotmail.com>
2022-09-29 13:56:36 +0200

    in progress

---
+++ src/js/components/Home.js
@@ -0,0 +1,18 @@
+class Home{
+  constructor(element){
+    const thisHome = this;
+    thisHome.render(element);
+    thisHome.initWidgets();
+  }
+
+  render(){
+
+  }
+
+  initWidgets(){
+
+  }
+
+}
+
+export default Home;
